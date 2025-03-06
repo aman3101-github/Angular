651a1e3e31bf1f09160bf9ffdf404988c47dc34c
Aman Prajapati <ap757162@gmail.com>
2025-03-06 10:42:13 +0530

    [Aman] Add. Hello From Bridgelabz using One Way data bindings.

--- HelloWorldApp/src/app/app.component.ts
+++ HelloWorldApp/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hello from BridgeLabz';
+  // Data property for Interpolation
+  title: string = "Hello from BridgeLabz"; 
 }
