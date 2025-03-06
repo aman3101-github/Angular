ffb22f6103f24295f8951db02859cd996da038cd
Aman Prajapati <ap757162@gmail.com>
2025-03-06 11:03:21 +0530

    [Aman] Add. Using Event Binding Launch Bridgelabz Website by Clicking on the logo.

--- HelloWorldApp/src/app/app.component.ts
+++ HelloWorldApp/src/app/app.component.ts
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string = "Welcome to BridgeLabz";
-  logoUrl: string = "https://imgs.search.brave.com/TKWOH1gzjT880iA72cMl2zTUFjhnIDwoSys4kjasSME/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMueW91cnN0b3J5/LmNvbS9jcy9pbWFn/ZXMvY29tcGFuaWVz/L2xvZ28tMTU4NjQx/OTU3NDMzNy5qcGc_/Zm09YXV0byZhcj0x/OjEmbW9kZT1maWxs/JmZpbGw9c29saWQm/ZmlsbC1jb2xvcj1m/ZmYmZm9ybWF0PWF1/dG8mdz0zODQmcT03/NQ";
+  logoUrl: string = "https://imgs.search.brave.com/nLlYCezxfnT6HHkFi0YroyfOF6r3eYTHIozTra5dZR4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/YnJpZGdlbGFiei5j/b20vYXNzZXRzL2lt/YWdlcy9CcmlkZ2VM/YWJ6JTIwTmV3JTIw/TG9nby5zdmc"; 
+  siteUrl: string = "https://www.bridgelabz.com"; 
+
+  // Method to open the BridgeLabz website in a new tab
+  openBridgeLabzSite() {
+    window.open(this.siteUrl, "_blank");
+  }
 }
