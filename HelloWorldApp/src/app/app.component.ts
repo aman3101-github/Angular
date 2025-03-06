35d65affbd523e0064fdc100a2ba60ce2d215444
Aman Prajapati <ap757162@gmail.com>
2025-03-06 10:50:55 +0530

    [Aman] Add. Display Bridgelabz Logo using Property binding technique.

--- HelloWorldApp/src/app/app.component.ts
+++ HelloWorldApp/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // Data property for Interpolation
-  title: string = "Hello from BridgeLabz"; 
+  title: string = "Welcome to BridgeLabz";
+  logoUrl: string = "https://imgs.search.brave.com/TKWOH1gzjT880iA72cMl2zTUFjhnIDwoSys4kjasSME/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMueW91cnN0b3J5/LmNvbS9jcy9pbWFn/ZXMvY29tcGFuaWVz/L2xvZ28tMTU4NjQx/OTU3NDMzNy5qcGc_/Zm09YXV0byZhcj0x/OjEmbW9kZT1maWxs/JmZpbGw9c29saWQm/ZmlsbC1jb2xvcj1m/ZmYmZm9ybWF0PWF1/dG8mdz0zODQmcT03/NQ";
 }
